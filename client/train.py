42ce9daed1847ae016ade004cc05d31758532413
jellyho <jellyho@naver.com>
2023-05-13 00:16:10 +0900

    Update train.py

--- client/train.py
+++ client/train.py
@@ -8,24 +8,25 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 import time, sys
 from RCServer import RCServer
+import cv2
 
 # do not modify
 app = QApplication(sys.argv)
-rc = RCServer()
+rc = RCServer(port=1234)
 
 ####
 class MainWorker(QObject):    
     @pyqtSlot()
     def main(self):
         # main training Code Here :)
+        self.capture = cv2.VideoCapture(1)
         while True:
-            while rc.available():
-                rc.worker.Act('A', 'W')
-                time.sleep(1)
-                rc.worker.Act('A', 'S')
-                time.sleep(1)
-            print('Robots are not available')
-            time.sleep(1)
+            ret, frame = self.capture.read()
+            if ret:
+                # OpenCV 프레임을 PyQt 이미지로 변환
+                rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                status = 'dreams come true'
+                rc.updateStatus(rgb_frame, status)
         return
  ####   
     
